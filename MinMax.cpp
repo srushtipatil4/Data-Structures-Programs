2677bfc4a6ef30f14eea4051e960e50a4d346031
srushtipatil4 <73375507+srushtipatil4@users.noreply.github.com>
2021-03-20 21:02:15 +0530

    Program to find Minimum and Maximum element of an array using minimum number of comparisons

---
+++ MinMax.cpp
@@ -0,0 +1,69 @@
+//Finding Minimum and Maximum of Array in minimum number of comparisons
+
+#include<iostream>
+using namespace std;
+int arr[10], n, i;
+struct pair
+{
+    int min;
+    int max;
+};
+
+struct pair getMinMax(int arr[], int n)
+{
+    struct pair minmax;
+    int i;
+    if (n%2 == 0)
+    {
+        if(arr[0] > arr[1])
+        {
+            minmax.max=arr[0];
+            minmax.min=arr[1];
+        }
+        else
+        {
+            minmax.max=arr[1];
+            minmax.min=arr[0];
+        }
+        i=2;
+    }
+    else
+    {
+        minmax.max=arr[0];
+        minmax.min=arr[0];
+        i=1;
+    }
+    
+    while(i < n-1)
+    {
+        if(arr[i]> arr[i+1])
+        {
+            if(arr[i] > minmax.max)
+                minmax.max = arr[i];
+            if(arr[i+1] < minmax.min)
+                minmax.min = arr[i+1]
+        }
+        else
+        {
+            if(arr[i+1] > minmax.max)
+                minmax.max = arr[i+1];
+            if(arr[i] < minmax.min)
+                minmax.min = arr[i];
+        }
+    }
+}
+
+int main()
+{
+    cout<<"Enter Size of Array: ";
+    cin>>n;
+    cout<<"Enter Elements of Array: ";
+    for(i=0; i<n; ++i)
+    {
+        cin>>arr[i];
+    }
+    pair minmax = getMinMax(arr, n);
+    cout<<"Minimum element of array: "<<minmax.min;
+    cout<<"Maximum element of array: "<<minmax.max;
+    return 0;
+}
